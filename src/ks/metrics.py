f72e51508ef96512edd03e8937cb94082361ef78
Airat-ASMPT-PC <airat.valiullin@asmpt.com>
2024-11-19 12:13:12 +0100

    add average LSD metric

--- src/ks/metrics.py
+++ src/ks/metrics.py
@@ -39,13 +39,36 @@ class LogSpectralDistance(Metric):
         psd = torch.abs(fft_result) ** 2 / n
         return psd[: n // 2 + 1]
 
-    def __call__(self, y_true: torch.Tensor, y_pred: torch.Tensor | None) -> torch.Tensor:
+    @classmethod
+    def log_distance(
+        cls, psd1: torch.Tensor, psd2: torch.Tensor, p: int
+    ) -> torch.Tensor:
+        assert psd2 is not None, "y_pred or mean_psd must be provided"
+        log_psd1 = torch.log(psd1 + cls._EPS)
+        log_psd2 = torch.log(psd2 + cls._EPS)
+        return torch.norm(log_psd1 - log_psd2, p=p)
+
+    def __call__(
+        self, y_true: torch.Tensor, y_pred: torch.Tensor | None
+    ) -> torch.Tensor:
         psd1 = self.compute_psd(y_true)
         psd2 = self.compute_psd(y_pred) if y_pred is not None else self.mean_psd
-        assert psd2 is not None, "y_pred or mean_psd must be provided"
-        log_psd1 = torch.log(psd1 + self._EPS)
-        log_psd2 = torch.log(psd2 + self._EPS)
-        return torch.norm(log_psd1 - log_psd2, p=self.p)
+        return self.log_distance(psd1, psd2, self.p)
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(p={self.p})"
+
+
+def evaluate_trajectory(traj: torch.Tensor, mean_spectrum: torch.Tensor) -> float:
+    # mean spectrum of the trajectory:
+    spectra = []
+    for u in traj:
+        spectra.append(LogSpectralDistance.compute_psd(u))
+
+    spectra = torch.stack(spectra)
+
+    mean_spectrum_traj = spectra.mean(dim=0)
+
+    return LogSpectralDistance.log_distance(
+        mean_spectrum_traj, mean_spectrum, p=2
+    ).item()
